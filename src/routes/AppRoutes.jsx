f34d354bb5e8bbac477024c2045c79dc50f312fa
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-03 15:58:12 +0100

    Rota a página sobre adicionada

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 //Site
 import Home from "../pages/site/Home";
-
+import Sobre from "../pages/site/Sobre";
 import NotFound from "../pages/site/NotFound";
 
 export default function AppRoutes() {
@@ -9,6 +9,7 @@ export default function AppRoutes() {
     <Routes>
       {/*Rotas do site */}
       <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
