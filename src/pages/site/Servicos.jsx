d698f31d8152eef599cab20f6b71849914fc71cc
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-03 16:23:43 +0100

    Página de serviços criada

---
+++ src/pages/site/Servicos.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import SobreImg from "../../assets/img/sobre.jpg";
+import SobreImg2 from "../../assets/img/sobre2.jpg";
+
+export default function Servicos() {
+  return (
+    <>
+      <title>Serviços | Nwayami</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
