713805644aff2537bf0253b04f219bb5ed6cfcaf
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-03 15:56:50 +0100

    Página sobre criada

---
+++ src/pages/site/Sobre.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import SobreImg from "../../assets/img/sobre.jpg";
+import SobreImg2 from "../../assets/img/sobre2.jpg";
+
+export default function Sobre() {
+  return (
+    <>
+      <title>Sobre nós | Nwayami</title>
+
+      {/* Header */}
+      <Header />
+
+      
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
