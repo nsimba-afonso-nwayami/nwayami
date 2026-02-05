29559f1b1a67ed86d6aa1c687d5f67844f2dcae0
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-05 09:22:25 +0100

    Rotas das páginas dos serviços de tecnologia  e consultoria  adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,6 +4,8 @@ import Home from "../pages/site/Home";
 import Sobre from "../pages/site/Sobre";
 import Servicos from "../pages/site/Servicos";
 import ServicosManutencaoPredial from "../pages/site/ServicosManutencaoPredial";
+import ServicosTecnologiaInformacao from "../pages/site/ServicosTecnologiaInformacao";
+import ServicosConsultoria from "../pages/site/ServicosConsultoria";
 import Projetos from "../pages/site/Projetos";
 import NotFound from "../pages/site/NotFound";
 
@@ -15,8 +17,8 @@ export default function AppRoutes() {
       <Route path="/sobre" element={<Sobre />} />
       <Route path="/servicos" element={<Servicos />} />
       <Route path="/servicos/manutencao-predial" element={<ServicosManutencaoPredial />} />
-      <Route path="/servicos/tecnologia-informacao" element={<Servicos />} />
-      <Route path="/servicos/consultoria" element={<Servicos />} />
+      <Route path="/servicos/tecnologia-informacao" element={<ServicosTecnologiaInformacao />} />
+      <Route path="/servicos/consultoria" element={<ServicosConsultoria />} />
       <Route path="/projetos" element={<Projetos />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
