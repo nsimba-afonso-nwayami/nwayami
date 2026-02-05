34c6b13fbf58f270d4cdc61fc5d62ea7e66f8b48
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-05 08:46:47 +0100

    Página projetos criada

---
+++ src/pages/site/Projetos.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Projetos() {
+  return (
+    <>
+      <title>Projetos | Nwayami</title>
+
+      {/* Header */}
+      <Header />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
