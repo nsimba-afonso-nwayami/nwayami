e349da90536472ab9b6774792bb4a9ce895fbf4e
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-09 09:31:44 +0100

    Componente SEO alterado

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -5,23 +5,31 @@ export default function SEO({
   description,
   keywords,
   image = "https://nwayami.vercel.app/seo/seo.jpg",
-  url = "https://nwayami.vercel.app/",
+  url,
 }) {
+  const currentUrl = url || window.location.href;
+
   return (
     <Helmet>
+      {/* Básico */}
       <title>{title}</title>
       <meta name="description" content={description} />
       {keywords && <meta name="keywords" content={keywords} />}
+      <link rel="canonical" href={currentUrl} />
 
       {/* Open Graph */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
-      <meta property="og:url" content={url} />
+      <meta property="og:url" content={currentUrl} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
     </Helmet>
   );
