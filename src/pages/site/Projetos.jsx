c595ef04bcff9e9daca44c7add043300a89f379d
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-02-09 08:18:23 +0100

    Imagem do banner fixa

--- src/pages/site/Projetos.jsx
+++ src/pages/site/Projetos.jsx
@@ -34,6 +34,7 @@ export default function Projetos() {
           backgroundImage: `url(${projetoBg})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
+          backgroundAttachment: "fixed",
         }}
       >
         <div className="absolute inset-0 bg-black/60"></div>
