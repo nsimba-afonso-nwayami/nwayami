19bcb17a59e5351c4bdd67e0082f50868f50aaf9
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-07-01 17:04:39 +0100

    Seção sobre adicionado na página home

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Hero from "@/components/home/Hero";
+import Sobre from "@/components/home/Sobre";
 
 // SEO GLOBAL DO SITE
 export const metadata = {
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <>
      <Hero />
+     <Sobre />
     </>
   );
 }
