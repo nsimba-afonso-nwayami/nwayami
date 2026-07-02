285d3e6fce5a124dd3e6b4862d7493b7a5ded7be
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-07-02 12:20:29 +0100

    Página quem somos criada

---
+++ src/app/quem-somos/page.js
@@ -0,0 +1,14 @@
+/*import Hero from "@/components/home/Hero";*/
+
+// SEO
+export const metadata = {
+  title: "Quem somos | Nwayami",
+  description: "Site da empresa de manutenção predial e construção civil",
+};
+
+export default function QuemSomos() {
+  return (
+    <>
+    </>
+  );
+}
