3bc61e163e4691e6cc3c7057d887771dce539e4d
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-07-16 16:41:37 +0100

    Banner de projetos feito

---
+++ src/components/projetos/BannerProjetos.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import BannerPage from "@/components/shared/BannerPage";
+import bannerProjetos from "@/assets/img/projetos.jpg";
+
+export default function BannerProjetos() {
+  return (
+    <BannerPage
+      image={bannerProjetos}
+      currentPage="Projetos"
+      title="Projetos que refletem"
+      highlight="qualidade e inovação."
+      description="Conheça alguns dos projetos que demonstram a nossa experiência, rigor técnico e compromisso com a excelência."
+    />
+  );
+}
